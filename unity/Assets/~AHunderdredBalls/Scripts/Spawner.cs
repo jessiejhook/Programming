d7f638e807cd35cf8107eb110e654f1a3dacea2e
jessica.hook8 <jessica.hook8@600900J211DE418>
2018-05-18 16:00:19 +1000

    Added A Hundred Balls

---
+++ unity/Assets/~AHunderdredBalls/Scripts/Spawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace AHundredBalls
+{
+    
+
+    public class Spawner : MonoBehaviour
+    {
+        public GameObject prefab; // Reference to object we want to spawn
+        public float spawnDelay = 1f; // Delay of spawn (in seconds)
+        // Use this for initialization
+        void Start()
+        {
+            // Start coroutine
+            StartCoroutine(Spawn());
+        }
+        
+        IEnumerator Spawn()
+        {
+            // Wait until spawn delay
+            yield return new WaitForSeconds(spawnDelay);
+
+            // Spawn an object
+            Instantiate(prefab, transform.position, transform.rotation);
+
+            // Start coroutine again
+            StartCoroutine(Spawn()); 
+        }
+        
+    }
+}
