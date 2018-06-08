a43855654dde6e31dbbd98b0641f174c11fec084
jessica.hook8 <jessica.hook8@600900J211DE418>
2018-06-08 15:34:58 +1000

    Removed subfolder

---
+++ Assets/~Assessment1/Tiled/FishTiled.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="FishTiled" tilewidth="128" tileheight="128" tilecount="64" columns="8">
+ <image source="../Sprites/fishSpritesheet@2.png" width="1136" height="1136"/>
+</tileset>
---
+++ Assets/~GoneHome/Scripts/Trigger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.Events;
+
+namespace GoneHome
+{
+    public class Trigger : MonoBehaviour
+    {
+        public UnityEvent onTriggered;
+        
+        // Fired off when another collider enters goal
+        void OnTriggerEnter(Collider other)
+        {
+            // Detect if other collider is player
+            if (other.name == "Player" &&
+                transform.childCount == 0)
+            {
+                // Fire off our event (onTriggered)
+                onTriggered.Invoke();
+            }
+        }
+    }
+}
